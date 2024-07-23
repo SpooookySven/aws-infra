236b75ee7ae0c75c82ce0a66f499fd09fd54bbc0
SpooookySven <sv.he@mailbox.org>
2024-07-23 15:52:44 +0200

    fix tf timeout issues

--- vpc-network/vpn_endpoint.tf
+++ vpc-network/vpn_endpoint.tf
@@ -12,14 +12,22 @@ resource "aws_ec2_client_vpn_endpoint" "milliways" {
   connection_log_options {
     enabled               = false
   }
+  tags = {
+    Name = var.name
+  }
+
 }
 
 resource "aws_ec2_client_vpn_network_association" "vpn_net_conn_pub" {
-	client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.milliways.id
-	for_each			   = {
-    for index, subnetID in module.vpc.public_subnets : "private-${module.vpc.azs[index]}" => subnetID
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.milliways.id
+  for_each			   = {
+  for index, subnetID in module.vpc.public_subnets : "private-${module.vpc.azs[index]}" => subnetID
   }
-	subnet_id              = each.value
+  subnet_id              = each.value
+  timeouts {
+    create = "30m"
+    delete = "30m"
+  }
 }
 
 resource "aws_ec2_client_vpn_route" "milliways_routing" {
@@ -29,17 +37,29 @@ resource "aws_ec2_client_vpn_route" "milliways_routing" {
     for index, subnetID in module.vpc.public_subnets : "private-${module.vpc.azs[index]}" => subnetID
   }
   target_vpc_subnet_id   = each.value
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
 }
 
 resource "aws_ec2_client_vpn_authorization_rule" "milliways_cidr_rule" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.milliways.id
   target_network_cidr    = var.cidr
   authorize_all_groups   = true
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
 }
 
 resource "aws_ec2_client_vpn_authorization_rule" "milliways_internet_rule" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.milliways.id
   target_network_cidr    = "0.0.0.0/0"
   authorize_all_groups   = true
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
 }
 
